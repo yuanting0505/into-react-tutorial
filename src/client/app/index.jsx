1c8e1dc9d183c922f7ba303a4d64f7681d03abe4
yuanting0505 <yuantingisme@gmail.com>
2017-03-26 19:04:50 +0800

    Take turns and render the turn in board

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -13,7 +13,8 @@ class Board extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			squares: Array(9).fill(null)
+			squares: Array(9).fill(null),
+			xIsNext: true
 		}
 	}
 	renderSquare(i) {
@@ -21,11 +22,14 @@ class Board extends React.Component {
 	}
 	handleClick(i) {
 		const squares = this.state.squares.slice();
-		squares[i] = 'X';
-		this.setState({ squares: squares });
+		squares[i] = this.state.xIsNext? 'X': 'O';
+		this.setState({ 
+			squares: squares, 
+			xIsNext: !this.state.xIsNext 
+		});
 	}
 	render() {
-		const status = 'Next player: X';
+		const status = 'Next player: ' + (this.state.xIsNext ? 'X': 'O');
 		return (
 			<div>
 				<div className="status">{status}</div>
